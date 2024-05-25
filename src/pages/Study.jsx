e3a5e1872fdc4ddc1cc6ea8a231040d7d3746168
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-05-25 17:21:29 +0530

    study updates

--- src/pages/Study.jsx
+++ src/pages/Study.jsx
@@ -1,38 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from "../components/nav2";
 import AlgorithmCard from "../components/ac";
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-
-// Components for each algorithm's detailed steps
-import RSADetails from '../components/RSADetails';
-import AESDetails from '../components/AESDetails';
-import DESDetails from '../components/DESDetails';
-import SHADetails from '../components/SHADetails';
 
 const Study = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route path="/study/rsa">
-            <RSADetails />
-          </Route>
-          <Route path="/study/aes">
-            <AESDetails />
-          </Route>
-          {/* Add similar routes for other algorithms */}
-          <Route path="/study">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
-              <Link to="/study/rsa">
-                <AlgorithmCard title="RSA" details="Click to view RSA steps." />
-              </Link>
-              {/* Repeat for other algorithm cards */}
-            </div>
-          </Route>
-        </Switch>
+    <div>
+      <Navbar />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
+        <Link to="/study/rsa">
+          <AlgorithmCard title="RSA" details="Click to view RSA steps." />
+        </Link>
+        <Link to="/study/aes">
+          <AlgorithmCard title="AES" details="Click to view AES steps." />
+        </Link>
+        <Link to="/study/des">
+          <AlgorithmCard title="DES" details="Click to view DES steps." />
+        </Link>
+        <Link to="/study/sha">
+          <AlgorithmCard title="SHA" details="Click to view SHA steps." />
+        </Link>
       </div>
-    </Router>
+    </div>
   );
 };
 
