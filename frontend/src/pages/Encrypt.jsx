cc19ac0b85c6c2a7693f490a2b6dc1d9d0b79953
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-06-26 20:29:57 +0530

    encrypt and decrypt ui fix

--- frontend/src/pages/Encrypt.jsx
+++ frontend/src/pages/Encrypt.jsx
@@ -4,6 +4,7 @@ import AESComponent from "../components/AESComponent";
 import RSAComponent from "../components/RSAComponent";
 import DSAEncryptComponent from "../components/DSAEncryptComponent";
 import SHAComponent from "../components/SHAComponent";
+import sharedClasses from "../styles/sharedClasses";
 
 function EncryptComponent() {
   const [encryptionType, setEncryptionType] = useState("aes");
@@ -11,47 +12,51 @@ function EncryptComponent() {
   return (
     <div className="flex flex-col min-h-screen bg-black text-white">
       <Navbar />
-      <div className="flex flex-col items-center p-6 bg-black min-h-screen text-white">
-        <h1 className="text-3xl font-bold mb-8">Encryption</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          <button
-            onClick={() => setEncryptionType("aes")}
-            className={`px-6 py-3 ${
-              encryptionType === "aes" ? "bg-blue-500" : "bg-gray-700"
-            } text-white rounded-md hover:bg-blue-600`}
-          >
-            AES
-          </button>
-          <button
-            onClick={() => setEncryptionType("rsa")}
-            className={`px-6 py-3 ${
-              encryptionType === "rsa" ? "bg-blue-500" : "bg-gray-700"
-            } text-white rounded-md hover:bg-blue-600`}
-          >
-            RSA
-          </button>
-          <button
-            onClick={() => setEncryptionType("dsa")}
-            className={`px-6 py-3 ${
-              encryptionType === "dsa" ? "bg-blue-500" : "bg-gray-700"
-            } text-white rounded-md hover:bg-blue-600`}
-          >
-            DES
-          </button>
-          <button
-            onClick={() => setEncryptionType("sha")}
-            className={`px-6 py-3 ${
-              encryptionType === "sha" ? "bg-blue-500" : "bg-gray-700"
-            } text-white rounded-md hover:bg-blue-600`}
-          >
-            SHA
-          </button>
-        </div>
-        <div className="mt-8">
-          {encryptionType === "aes" && <AESComponent />}
-          {encryptionType === "rsa" && <RSAComponent />}
-          {encryptionType === "dsa" && <DSAEncryptComponent />}
-          {encryptionType === "sha" && <SHAComponent />}
+      <div className={`${sharedClasses.container} flex-grow pt-0`}>
+        <div className="container mx-auto px-1 py-2 mt-0">
+          <h1 className="text-3xl font-bold text-center dark:text-zinc-100">
+            Encryption
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
+            <button
+              onClick={() => setEncryptionType("aes")}
+              className={`px-6 py-3 ${
+                encryptionType === "aes" ? "bg-blue-500" : "bg-gray-700"
+              } text-white rounded-md hover:bg-blue-600`}
+            >
+              AES
+            </button>
+            <button
+              onClick={() => setEncryptionType("rsa")}
+              className={`px-6 py-3 ${
+                encryptionType === "rsa" ? "bg-blue-500" : "bg-gray-700"
+              } text-white rounded-md hover:bg-blue-600`}
+            >
+              RSA
+            </button>
+            <button
+              onClick={() => setEncryptionType("dsa")}
+              className={`px-6 py-3 ${
+                encryptionType === "dsa" ? "bg-blue-500" : "bg-gray-700"
+              } text-white rounded-md hover:bg-blue-600`}
+            >
+              DES
+            </button>
+            <button
+              onClick={() => setEncryptionType("sha")}
+              className={`px-6 py-3 ${
+                encryptionType === "sha" ? "bg-blue-500" : "bg-gray-700"
+              } text-white rounded-md hover:bg-blue-600`}
+            >
+              SHA
+            </button>
+          </div>
+          <div className="mt-8">
+            {encryptionType === "aes" && <AESComponent />}
+            {encryptionType === "rsa" && <RSAComponent />}
+            {encryptionType === "dsa" && <DSAEncryptComponent />}
+            {encryptionType === "sha" && <SHAComponent />}
+          </div>
         </div>
       </div>
     </div>
