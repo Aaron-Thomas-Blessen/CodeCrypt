714789b5cd90415ae4661bf3bc81cd2df36d3b8f
Aaron Thomas Blessen <85331681+Aaron-Thomas-Blessen@users.noreply.github.com>
2024-07-16 16:01:39 +0530

    Update Firebase.jsx

--- frontend/src/Firebase/Firebase.jsx
+++ frontend/src/Firebase/Firebase.jsx
@@ -1,24 +1,5 @@
-// Firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
+//you have to setup a Firebase account
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCdNo6VFMKCcgOx8t1SU6ZzFpyqe1P54a0",
-  authDomain: "codecrypt-b591f.firebaseapp.com",
-  projectId: "codecrypt-b591f",
-  storageBucket: "codecrypt-b591f.appspot.com",
-  messagingSenderId: "778461058311",
-  appId: "1:778461058311:web:fa4d6a767346e1438821a2",
-  measurementId: "G-R2C510JXC3",
-};
+//set up tables according to readme file.
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
+//copy and paste your firebase api here.
